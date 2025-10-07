a34b289e924996c2b35c8aa5891b1b47df256874
HappyTanuki <rlaalstn120@icloud.com>
2025-10-08 05:23:36 +0900

    일단 빌드도 되고 실행도 되는데 디펜더에 막힘.. aes도 막히는지 봐야 함
    만약 aes도 막히면 그냥 소프트웨어 구현에서 만족해야 할 듯.. 앱 서명 비용은 보나마나 할 때마다 100만원 이딴식일거라..

--- src/util/cpu_detect.cc
+++ src/util/cpu_detect.cc
@@ -1,14 +1,26 @@
 #include "util/cpu_detect.h"
 
+#ifndef _WIN32
 #include <cpuid.h>
+#else
+#include <intrin.h>
+#endif
 
 namespace file_encrypt::util {
 
 EdxEcx GetCPUFeatures() {
   EdxEcx features;
+#ifndef _WIN32
   std::uint32_t eax, ebx;
   __get_cpuid(1, &eax, &ebx, &features.ecx, &features.edx);
   return features;
+#else
+  int cpuInfo[4] = {0};
+  __cpuid(cpuInfo, 1);
+  features.edx = static_cast<std::uint32_t>(cpuInfo[3]);
+  features.ecx = static_cast<std::uint32_t>(cpuInfo[2]);
+  return features;
+#endif
 }
 
 }  // namespace file_encrypt::util
