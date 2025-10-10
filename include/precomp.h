6cc126641dcad4323964db96127ceaea5084ac9f
HappyTanuki <HappyTanuki@icloud.com>
2025-10-10 14:49:43 +0900

    매크로 개선

--- include/precomp.h
+++ include/precomp.h
@@ -1,9 +1,9 @@
 #ifndef FILE_ENCRYPT_UTIL_INCLUDE_PRECOMP_H
 #define FILE_ENCRYPT_UTIL_INCLUDE_PRECOMP_H
 #if _WIN32
-#include <winsock2.h>
+	#include <winsock2.h>
 #else
-#include <arpa/inet.h>
+	#include <arpa/inet.h>
 #endif
 #include <immintrin.h>
 
@@ -12,5 +12,12 @@
 #include <cstdint>
 #include <iostream>
 #include <vector>
+
+#if _WIN32
+	// because microsoft smart app control blocks
+	#define _USE_SIMD_INTRINSIC false 
+#else
+	#define _USE_SIMD_INTRINSIC true
+#endif
 
 #endif
